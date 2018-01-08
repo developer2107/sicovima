5b868e4bb0d06dd072832b6486b7ba3fa771f506
brendacg <33129048+brendacg@users.noreply.github.com>
2018-01-07 21:37:41 -0600

    compra.js
    compra

--- public/js/scripts/compras.js
+++ public/js/scripts/compras.js
@@ -1,46 +1,40 @@
-//Brendaly Carmela
-function agregarProducto(){
 
-  var cantidad=$("#cant_DCom").val();
-  var tipo=$("#tipo_MP").val();
-  var medida=$("#unidadMedida_MP").val();
-  var color=$("#color_MP").val();
-  var nombre=$("#nombre_MP").val();
-  var subTotal=$("#subTotal_DCom").val();
+var id_MP;
+var nombre_MP;
+var precio_MP;
+var color_MP;
 
-  var a = parseFloat(cantidad);
-  var b = parseFloat(subTotal);
-  var total = a*b;
+$(document).on('ready',function(){
 
-  //var total1 = parseFloat(total);
-  var suma1 =  parseFloat( $('#total_Com').val());
-  var suma = parseFloat(total);
+$('#tablaMateriaPrima').on('click','#AddCant',function(e){
+  nombre_MP=$(this).parents('tr').find('input:eq(0)').val();
+  precio_MP=$(this).parents('tr').find('input:eq(2)').val();
+  color_MP=$(this).parents('tr').find('input:eq(1)').val();
+  id_MP=$(this).parents('tr').find('input:eq(3)').val();
+  $("#agregarCompras").val("Agregar");
+  $("#cantidad").val("");
+});
 
-  var suma1 = total+suma1;
-  $('#total_Com').val(suma1);
+$('#agregarCompras').click(function(){
+  var total = parseFloat($("#total_Com").val());
+  var accion = $("#agregarCompras").val();
+  if (accion == "Agregar") {
+    var cantidad=$("#cantidad").val();
+    var c = parseFloat(cantidad)*parseFloat(precio_MP);
+    total = total + c;
+    $("#total_Com").val(parseFloat(total).toFixed(2));
 
-
-  $("#Compra_producto").append("<tr>"+
-    "<td><input type='hidden' name='total[]' value='"+total+"'>"+
-    "<input type='hidden' name='cantidadc[]' value='"+cantidad+"'>"+
-    "<input type='hidden' name='tipoc[]' value='"+tipo+"'>"+
-    "<input type='hidden' name='medidac[]' value='"+medida+"'>"+
-    "<input type='hidden' name='colorc[]' value='"+color+"'>"+
-    "<input type='hidden' name='nombrec[]' value='"+nombre+"'>"+
-    "<input type='hidden' name='subTotalc[]' value='"+subTotal+"'>"+
-    "</td><td>"+cantidad+"</td><td>"+nombre+"</td><td>"+parseFloat(total).toFixed(2)+"</td><td class='deleteCompra' style='cursor:pointer;'>Eliminar</td></tr>");
-  //Limpiar campos
-  $("#cant_DCom").val("");
-  $("#tipo_MP").val("");
-  $("#unidadMedida_MP").val("");
-  $("#color_MP").val("");
-  $("#nombre_MP").val("");
-  $("#subTotal_DCom").val("");
-}
-
+    $("#Compra_producto").append("<tr>"+
+      "<td><input type='hidden' name='cantidadc[]' value='"+cantidad+"'>"+
+      "<input type='hidden' name='idc[]' value='"+id_MP+"'>"+
+      "<input type='hidden' name='subTotalc[]' value='"+c+"'>"+
+      "</td><td>"+cantidad+"</td><td>"+nombre_MP+" "+color_MP+"</td><td>"+parseFloat(precio_MP).toFixed(2)+"</td><td>"+parseFloat(c).toFixed(2)+"</td><td class='deleteCompra' style='cursor:pointer;'>Eliminar</td></tr>");
+  }
+});
+});
 
 $(document).on("click",".deleteCompra",function(){
-var totalF=parseFloat($(this).parents('tr').find('td:eq(3)').html());
+var totalF=parseFloat($(this).parents('tr').find('td:eq(4)').html());
 var parent = $(this).parents().get(0);
 
 $(parent).remove();
@@ -97,108 +91,3 @@ var total=parseFloat($("#total_Com").val());
     total=total-(totalF);
      document.getElementById("total_Com").value=total.toFixed(2);
 });
-
-
-// function guardarProveedor(){
-//     vnombre=validarNombre();
-//     vcorreo=validarCorreo();
-//     vtelefono=validarTelefono();
-//     if(vnombre==false || vcorreo==false || vtelefono==false){
-//
-//     }else{
-//       alert("Todo bien");
-//     }
-//
-// }
-
-// function validarNombre(){
-//   nombre=$("#nombre").val();
-//   val= true;
-//     cont=0;
-//
-//     var errores= [];
-//     if(nombre!=""){
-//       $.get('/blissey/public/existeNombreProveedor/'+nombre,function(data){
-//         if(data=="true"){
-//
-//         }
-//       });
-//     }
-//     if(nombre==""){
-//       errores[cont]="El campo drogería es obligatorio";
-//
-//       cont=cont+1;
-//     }else if(nombre.length<5){
-//       errores[cont]="El campo drogería debe contener mínimo 5 caracteres";
-//       cont=cont+1;
-//     }else if(nombre.length>50){
-//       errores[cont]="El campo drogería debe contener máximo 50 caracteres";
-//       cont=cont+1;
-//     }
-//     if(cont>0){
-//       val= false;
-//     }
-//     for(a=0;a<cont;a++){
-//       new PNotify({
-//         title: 'Error!',
-//         text: errores[a],
-//         type: 'error',
-//         styling: 'bootstrap3'
-//       });
-//     }
-//     return val;
-// }
-//
-// function validarCorreo(){
-//   correo=$("#correo").val();
-//   val= true;
-//     cont=0;
-//     var errores= [];
-//     if(correo==""){
-//       errores[cont]="El campo correo es obligatorio";
-//       cont=cont+1;
-//     }
-//     if(cont>0){
-//       val= false;
-//     }
-//     for(a=0;a<cont;a++){
-//       new PNotify({
-//         title: 'Error!',
-//         text: errores[a],
-//         type: 'error',
-//         styling: 'bootstrap3'
-//       });
-//     }
-//     return val;
-// }
-// function validarTelefono(){
-//   telefono=$("#telefono").val();
-//   val= true;
-//     cont=0;
-//     var errores= [];
-//     if(telefono==""){
-//       errores[cont]="El campo teléfono es obligatorio";
-//       cont=cont+1;
-//     }else if(telefono.length!=9){
-//       errores[cont]="El campo teléfono debe contener 9 caracteres";
-//       cont=cont+1;
-//     }
-//     if(cont>0){
-//       val= false;
-//     }
-//     for(a=0;a<cont;a++){
-//       new PNotify({
-//         title: 'Error!',
-//         text: errores[a],
-//         type: 'error',
-//         styling: 'bootstrap3'
-//       });
-//     }
-//     return val;
-// }
-// function sleep(miliseconds) {
-//    var currentTime = new Date().getTime();
-//
-//    while (currentTime + miliseconds >= new Date().getTime()) {
-//    }
-// }
